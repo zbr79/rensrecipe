d702a2357aff9bd8ed88433a6b2b5917b85b79b6
Zhibin Ren <zhibinren79@gmail.com>
2024-02-27 04:08:39 -0800

    created side bar

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidenavbar')
+    sidebar.style.display = 'flex';
+} 
+
+function closeSidebar(){
+    const Sidebar = document.querySelector('.sidenavbar')
+    Sidebar.style.display = 'none';
+} 
