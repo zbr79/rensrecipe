37e0853822824aa71df86e9af9fab8fb8e39cc3e
Zhibin Ren <zhibinren79@gmail.com>
2024-02-28 05:05:21 -0800

    experiment change
    this is where 228backup is created

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 function showSidebar(){
-    const sidebar = document.querySelector('.sidenavbar')
-    sidebar.style.display = 'flex';
+  const sidebar = document.querySelector('.navbar')
+  sidebar.style.display = 'flex';
 } 
 
 function closeSidebar(){
-    const Sidebar = document.querySelector('.sidenavbar')
-    Sidebar.style.display = 'none';
+  const Sidebar = document.querySelector('.navbar')
+  Sidebar.style.display = 'none';
 } 
+
+
+function changeImage() {
+  var image = document.getElementById("header-navbar-button");
+
+  // Check the current source and change it to the other image
+  if (image.src.match("img/icon/menu.svg")) {
+    image.src = "img/icon/close.svg";
+    const sidebar = document.querySelector('.navbar')
+    sidebar.style.display = 'flex';
+  } else {
+    image.src = "img/icon/menu.svg";
+    const Sidebar = document.querySelector('.navbar')
+    Sidebar.style.display = 'none';
+  }
+}
