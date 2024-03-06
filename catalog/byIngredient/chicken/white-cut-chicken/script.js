732f46ce9dde944b7b1f5403e635cfb1273af5d8
Zhibin Ren <zhibinren79@gmail.com>
2024-03-06 00:45:11 -0800

    update white cut chicken page

--- catalog/byIngredient/chicken/white-cut-chicken/script.js
+++ catalog/byIngredient/chicken/white-cut-chicken/script.js
@@ -1,22 +1,42 @@
 
-  
-  function changeImage() {
-    // console.log('called');
-    var image = document.getElementById("header-navbar-button");
-  
-    // Check the current source and change it to the other image
-    if (image.src.match("img/icon/menu.svg")) {
-      image.src = "../../../../img/icon/close.svg";
-      const sidebar = document.querySelector('.navbar')
-      sidebar.style.display = 'flex';
+// scipt for top bar's sidebar buttton picture change
+function changeImage() {
+  var image = document.getElementById("sidebar-button").querySelector('img');
+  console.log(image);
 
-    } else {
-
-      image.src = "../../../../img/icon/menu.svg";
-      const Sidebar = document.querySelector('.navbar')
-      Sidebar.style.display = 'none';
-    }
+  // Check the current source and change it to the other image
+  if (image.src.match("IMG/icon/menu.svg")) {
+    image.src = "/IMG/icon/close.svg";
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex';
+  } else {
+    image.src = "/IMG/icon/menu.svg";
+    const Sidebar = document.querySelector('.sidebar')
+    Sidebar.style.display = 'none';
   }
+}
+
+// script for sidebar's toggle
+function changeSideBar(chart,button) {
+
+  var flexSection = document.getElementById(chart);
+
+  const Fbutton = document.getElementById(button);
+
+  var computedStyle = window.getComputedStyle(flexSection);
+
+
+  if (computedStyle.display === 'block') {
+      flexSection.style.display = 'none';
+      Fbutton.src = "/IMG/icon/arrow-right.svg";
+      
+  } else {
+      flexSection.style.display = 'block';
+      Fbutton.src = "/IMG/icon/down.svg";
+ 
+
+  }
+}
 
   function changeChart(chart,button) {
 
@@ -29,13 +49,11 @@
 
     if (computedStyle.display === 'block') {
         flexSection.style.display = 'none';
-        Fbutton.src = "../../../../img/icon/down.svg";
+        Fbutton.src = "/IMG/icon/down.svg";
         
     } else {
         flexSection.style.display = 'block';
-        Fbutton.src = "../../../../img/icon/up.svg";
-   
-
+        Fbutton.src = "/IMG/icon/up.svg";
     }
 }
 
